e73fdb1f159d8c425d7a1ea92449c171215d6fcd
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-10 22:04:01 -0400

    added day to schema

--- models/schema.js
+++ models/schema.js
@@ -1,14 +1,43 @@
 // Setting up Mongoose requirement for schema;
 const mongoose = require("mongoose");
-                console.log(mongoose);
+                console.log("This is mongoose", mongoose);
 // Setting up Schema variable using mongoose.Schema;
 const Schema = mongoose.Schema;
-                console.log(Schema);
+                console.log("This is Schema in mongoose", Schema);
 
 // Setting up Schema to be used as a requirement;
 const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    }, 
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please, enter the type of exercise."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please, enter the exercise's name."
+            },
+            duration: {
+                
+            },
+            weight: {
 
-// })
+            },
+            reps: {
+
+            },
+            sets: {
+
+            }
+        }
+    ]
+})
 
 // Setting up Schema as a model;
 // const Workout = mongoose.model("Workout", workoutSchema);
