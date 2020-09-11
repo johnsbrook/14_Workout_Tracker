a5265cb4761ab5f8955c8e23000c585d59f882ff
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-09-10 20:55:17 -0400

    established file structure

---
+++ models/schema.js
@@ -0,0 +1,17 @@
+// Setting up Mongoose requirement for schema;
+const mongoose = require("mongoose");
+                console.log(mongoose);
+// Setting up Schema variable using mongoose.Schema;
+const Schema = mongoose.Schema;
+                console.log(Schema);
+
+// Setting up Schema to be used as a requirement;
+const workoutSchema = new Schema({
+
+// })
+
+// Setting up Schema as a model;
+// const Workout = mongoose.model("Workout", workoutSchema);
+
+// Exporting model;
+// module.exports = Workout;
